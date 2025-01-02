2f224f282e4bdca5902cf40d47ec1892ed66c584
Dewax kuxi <131457111+Jalunajsjs@users.noreply.github.com>
2025-01-02 15:44:28 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ sleep 5s;
 
 docker run --rm -i \
   --shm-size 8g \
-  dydarsfhjjhss/newtestmltb001:charantejproject01
+  dydarsfhjjhss/rcmktbcloudclone999:charantejproject02
 
 # start the script
 exec "$@"
